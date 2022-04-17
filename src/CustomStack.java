2ec2a7b25764e36d2a16ed6109917c220ef04f33
VadimBerezyak <89918671+VadimBerezyak@users.noreply.github.com>
2022-04-17 16:58:26 +0300

    first method addElement: it's work!

--- src/CustomStack.java
+++ src/CustomStack.java
@@ -1,53 +1,56 @@
 import java.util.Arrays;
 
 public class CustomStack {
-    private static Object customNull = "Super";
+
+    private static final Object customNull = "SUPERNULL";
+    private static Object[] sourceArray = {null};
+    private static Object[] resultArray = new Object[sourceArray.length];
+
     public static void main(String[] args) {
-        addElement("123", "Super");
+        addElement("123", customNull, sourceArray);
     }
 
-    private static Object[] sourceArray = {1,2,3,123, 123, "Super", null, null};
-    private static Object[] resultArray = new Object[sourceArray.length];
-    // private static Object[] resultArray = new Object[(int) Math.ceil(sourceArray.length * 1.5)];
-
-    public static void addElement(Object str, Object customNull) {
-        for (int i = 0; i <sourceArray.length ; i++) {
+    public static void addElement(Object str, Object customNull, Object[] sourceArray) {
+        for (int i = 0; i < sourceArray.length; i++) {
             resultArray[i] = sourceArray[i];
         }
         if (sourceArray[sourceArray.length - 1] != customNull && sourceArray[sourceArray.length - 1] != null) {
             Object[] resultArray = new Object[(int) Math.ceil(sourceArray.length * 1.5)];
-            for (int i = 0; i <sourceArray.length ; i++) {
+            for (int i = 0; i < sourceArray.length; i++) {
                 resultArray[i] = sourceArray[i];
             }
             resultArray[sourceArray.length] = str;
-            if( resultArray[resultArray.length -1] == null) {
-                resultArray[sourceArray.length+1] = customNull;
+            if (resultArray[resultArray.length - 1] == null) {
+                resultArray[sourceArray.length + 1] = customNull;
             }
             sourceArray = resultArray;
             System.out.println(Arrays.toString(sourceArray));
-        }else if(sourceArray[sourceArray.length - 1] == customNull){
+        } else if (sourceArray[sourceArray.length - 1] == customNull) {
             Object[] resultArray = new Object[(int) Math.ceil(sourceArray.length * 1.5)];
-            for (int i = 0; i <sourceArray.length ; i++) {
+            for (int i = 0; i < sourceArray.length; i++) {
                 resultArray[i] = sourceArray[i];
             }
-            resultArray[sourceArray.length-1] = str;
+            resultArray[sourceArray.length - 1] = str;
             resultArray[sourceArray.length] = customNull;
             sourceArray = resultArray;
             System.out.println(Arrays.toString(sourceArray));
-        }
-        else if(sourceArray[sourceArray.length - 1] == null){
+        } else if (sourceArray[sourceArray.length - 1] == null && sourceArray.length != 1) {
             for (int i = 0; i < sourceArray.length; i++) {
-                if (sourceArray[i] == customNull){
+                if (sourceArray[i] == customNull) {
                     resultArray[i] = str;
-                    resultArray[i+1] = customNull;
+                    resultArray[i + 1] = customNull;
                     sourceArray = resultArray;
                     break;
                 }
                 sourceArray = resultArray;
             }
-
             System.out.println(Arrays.toString(sourceArray));
 
+        } else if (sourceArray[sourceArray.length - 1] == null && sourceArray.length == 1) {
+            resultArray[0] = str;
+            sourceArray = resultArray;
+            System.out.println(Arrays.toString(sourceArray));
         }
     }
 }
+
